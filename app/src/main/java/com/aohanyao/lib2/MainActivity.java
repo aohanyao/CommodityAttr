a633369fd319755026dd9c04001412cf72f1f1ff
JJC <928692385@qq.com>
2017-09-20 17:19:11 +0800

    删除多余的库

--- app/src/main/java/com/aohanyao/lib2/MainActivity.java
+++ app/src/main/java/com/aohanyao/lib2/MainActivity.java
@@ -3,27 +3,18 @@ package com.aohanyao.lib2;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
 
 import com.aohanyao.commodity.library.CommoditySpiderHelper;
 import com.aohanyao.commodityattr.mvp.contract.CommodityContract;
 import com.aohanyao.commodityattr.mvp.presenter.CommodityPresenter;
-import com.bigkoo.convenientbanner.ConvenientBanner;
 
 import java.util.Map;
 import java.util.TreeMap;
 
 import aohanyao.com.commodityattr.R;
-import butterknife.Bind;
-import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements CommodityContract.View {
-    @Bind(R.id.iv_shop_photo)
-    ConvenientBanner convenientBanner;
-    @Bind(R.id.tv_shop_price)
-    TextView tvShopPrice;
-    @Bind(R.id.tv_shop_name)
-    TextView tvShopName;
+
     private CommoditySpiderHelper mCommoditySpiderHelper;
     private CommodityPresenter presenter;
 
@@ -31,10 +22,6 @@ public class MainActivity extends AppCompatActivity implements CommodityContract
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-
-
-        //initData();
 
 
     }
@@ -64,7 +51,6 @@ public class MainActivity extends AppCompatActivity implements CommodityContract
     @Override
     protected void onStart() {
         super.onStart();
-        convenientBanner.startTurning(3000);
     }
 
 }
